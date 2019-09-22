1d501134e867080b22394a62bcd084b1e583e933
vinay-lanka <lanka.vinay2017@vitstudent.ac.in>
2019-09-22 10:37:38 +0530

    Design touches

--- public/js/script.js
+++ public/js/script.js
@@ -18,28 +18,30 @@ app.controller('control', function($scope,$http) {
       console.log(y);
       console.log(event.target.id);
       $scope.coord.push({x,y});
-      console.log(document.getElementById(event.target.id).style.backgroundColor=="white");
-      //if ($scope.color == "white")
+      // console.log(document.getElementById(event.target.id).style.backgroundColor=="white");
       document.getElementById(event.target.id).style.backgroundColor="blue";
       if(document.getElementById(event.target.id).style.backgroundColor=="white")
       $scope.color = "blue";
-      //$(event.target.id).css({"background-color":"blue"});
     }
     $scope.submitFunc = function() {
       console.log($scope.coord);
-      $http({
-        url: "/submit",
-        method: "POST",
-        data: $scope.coord
-      }).then(function successCallback(response) {
-              // this callback will be called asynchronously
-              // when the response is available
-              console.log("LOL");
-              $scope.data = response.data;
-          }, function errorCallback(response) {
-              // called asynchronously if an error occurs
-              // or server returns response with an error status.
-              $scope.error = response.statusText;
-      });
+      if($scope.coord.length==20){
+        $http({
+          url: "/submit",
+          method: "POST",
+          data: $scope.coord
+        }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+                console.log("LOL");
+                $scope.data = response.data;
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                $scope.error = response.statusText;
+        });
+      }else{
+        alert("Please enter 20 co-ordinates!")
+      }
     }
 });
